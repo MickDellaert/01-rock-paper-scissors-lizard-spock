197a6b8552aa637f34f6db36ad83b89bf03b60b2
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-04-28 21:48:54 +0200

    better funtionality - needs work on score

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const lizard = document.getElementById("lizard");
 const spock = document.getElementById("spock");
-const compRandom = document.getElementById("start");
+const startButton = document.getElementById("start");
 const resetButton = document.getElementById("reset");
 let playerChoice;
 
@@ -11,32 +11,39 @@ resetButton.addEventListener("click", function () {
     location.reload();
 });
 
+startButton.addEventListener("click", computer);
+
 rock.addEventListener("click", () => {
     playerChoice = rock.innerHTML;
     document.getElementById("user-choice").innerHTML = playerChoice;
+    setTimeout(() => { computer(); }, 400);
 });
 
 paper.addEventListener("click", () => {
     playerChoice = paper.innerHTML;
     document.getElementById("user-choice").innerHTML = playerChoice;
+    setTimeout(() => { computer(); }, 400);
 });
 
 scissors.addEventListener("click", () => {
     playerChoice = scissors.innerHTML;
     document.getElementById("user-choice").innerHTML = playerChoice;
+    setTimeout(() => { computer(); }, 400);
 });
 
 lizard.addEventListener("click", () => {
     playerChoice = lizard.innerHTML;
     document.getElementById("user-choice").innerHTML = playerChoice;
+    setTimeout(() => { computer(); }, 400);computer();
 });
 
 spock.addEventListener("click", () => {
     playerChoice = spock.innerHTML;
     document.getElementById("user-choice").innerHTML = playerChoice;
+    setTimeout(() => { computer(); }, 400);
 });
 
-compRandom.addEventListener("click", () => {
+function computer () {
     let choices = [rock.innerHTML, paper.innerHTML, scissors.innerHTML, lizard.innerHTML, spock.innerHTML];
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
@@ -46,7 +53,6 @@ compRandom.addEventListener("click", () => {
     console.log(computerChoice);
     console.log(playerChoice);
 
-    function compare() {
         if (computerChoice === playerChoice) {
             document.getElementById("result").innerHTML = "it's a tie";
         } else if (computerChoice === "✊") {
@@ -84,9 +90,8 @@ compRandom.addEventListener("click", () => {
                 document.getElementById("result").innerHTML = "player wins";
             }
         }
-    }
-    compare();
-});
+
+}
 
 
 
